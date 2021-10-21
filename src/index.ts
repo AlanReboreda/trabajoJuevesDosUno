ea90dcca6e2341a76a36edbad828bed9e09b1267
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-21 20:52:49 -0300

    termineeEl2 otroEjercicio

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let contador: number = 1;
+/*let contador: number = 1;
 let clave: string;
 
 while (contador <= 3 && clave != "eureka") {
@@ -9,4 +9,16 @@ if (clave == "eureka") {
   console.log("clave correcta");
 } else {
   console.log("ya no tienes intentos");
+}*/
+
+//2
+/*let indice: number;
+let cuenta: string = "";
+for (let indice: number = 1; indice <= 100; indice++) {
+  if (indice % 2 == 0 || indice % 3 == 0) {
+    cuenta = cuenta + indice + "";
+  }
 }
+console.log("cuenta");*/
+
+//3
